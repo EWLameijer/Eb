99de49698068cd754a7ab84e5a5b463bd3864560
EWLameijer <eric_wubbo@hotmail.com>
2016-02-04 07:58:42 +0100

    Redefining Eb 2/3

--- Feedback.java
+++
@@ -1,8 +0,0 @@
-package learning_software;
-
-class Feedback {
-	private int m_quantitativeFeedback; 
-	public long getNewInterval(History history) {
-		return 1;
-	}
-}
--- Review.java
+++
@@ -1,9 +0,0 @@
-package learning_software;
-
-import java.util.Date;
-
-public class Review {
-	Date m_timepoint;
-	double m_thinkingTime;
-	Feedback m_feedback;
-}
